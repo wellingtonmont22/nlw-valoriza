45920361ef586311fe51fc277f4bf37c4903e709
wellingtonmont22 <wellington.m.alves@hotmail.com>
2021-06-24 11:45:41 -0300

    Serviço de Criação de Usuários

---
+++ src/services/CreateUsersServices.ts
@@ -0,0 +1,29 @@
+import { UsersRepositories } from "../repositories/UsersRepositories";
+
+interface IUserRequest{
+    name: string,
+    email: string,
+    admin?: boolean
+}
+
+export class CreateUsersServices{
+    async execute({name,email,admin}: IUserRequest){
+
+        const usersRepositories = new UsersRepositories
+
+        const userAlreadyExists = await usersRepositories.findOne({email})
+        if(!email){
+            throw new Error("Email incorrect")
+        }
+
+        if(userAlreadyExists){
+            throw new Error("User already exists")
+        }
+
+        const user = usersRepositories.create({name,email,admin})
+
+        await usersRepositories.save(user)
+
+        return user
+    }
+}
